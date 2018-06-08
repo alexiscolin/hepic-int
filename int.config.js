9e6b1ac97c7d457a456abf377dbee5dd6e123d81
Alexis Colin <nesquimo@gmail.com>
2018-06-08 10:40:41 +0200

    pug template in webpack

--- int.config.js
+++ int.config.js
@@ -3,17 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     int: './int/src/js/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Managemente'
+      title: 'Hepic int',
+      template: './int/index.pug',
     })
   ],
   output: {
     path: path.resolve(__dirname + '/int', 'dist'),
     filename: './[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      }
+    ]
   }
 };
