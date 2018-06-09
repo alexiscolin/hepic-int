775bba4db860c8aefa4d2d21881e91148ba6d985
Alexis Colin <nesquimo@gmail.com>
2018-06-09 08:38:04 +0200

    Mod: gulp config
    del gulp build file when precess, int assets and pug layout structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,26 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const less = require('gulp-less');
 const minifyCSS = require('gulp-csso');
+const del = require('del');
+
+gulp.task('clean', function(){
+  del('int/build/**');
+});
 
 gulp.task('html', function(){
-  return gulp.src('int/*.pug')
+  del('int/build/**', {force:true});
+  return gulp.src('int/src/pages/*.pug')
     .pipe(pug())
-    .pipe(gulp.dest('int/build/html'))
+    .pipe(gulp.dest('int/build'))
 });
 
 gulp.task('css', function(){
+  del('int/build/**', {force:true});
   return gulp.src('int/src/style/*.less')
     .pipe(less())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('int/build/css'))
+    .pipe(gulp.dest('int/build/dist/css'))
 });
 
 
-gulp.task('default', [ 'html', 'css']);
+gulp.task('default', ['clean', 'html', 'css']);
