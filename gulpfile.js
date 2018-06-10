9cbd454e5b9643cad4e8ec94efcb43e3f683af89
Alexis Colin <nesquimo@gmail.com>
2018-06-10 11:44:04 +0200

    Add livereload on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,44 @@ const pug = require('gulp-pug');
 const less = require('gulp-less');
 const minifyCSS = require('gulp-csso');
 const del = require('del');
+const bs = require('browser-sync').create();
+
+const int = {
+  build: "./int/build",
+  src: "./int/src"
+};
 
 gulp.task('clean', function(){
-  del.sync('int/build/**');
+  del.sync(int.build + '/**');
 });
 
 gulp.task('html', function(){
-  return gulp.src('int/src/pages/*.pug')
+  return gulp.src(int.src + '/pages/*.pug')
     .pipe(pug())
-    .pipe(gulp.dest('int/build'))
+    .pipe(gulp.dest(int.build))
 });
 
 gulp.task('css', function(){
-  return gulp.src('int/src/assets/style/*.less')
+  return gulp.src(int.src + '/assets/style/*.less')
     .pipe(less())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('int/build/assets/css'))
+    .pipe(gulp.dest(int.build + '/assets/css'))
+    .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('sync', function() {
+    bs.init({
+        server: {
+            baseDir: int.build
+        }
+    });
 });
 
 
 gulp.task('default', ['clean', 'html', 'css']);
+
+gulp.task('watch', ['sync'], function(){
+  gulp.watch(int.src + '/assets/style/*.less', ['css']);
+  gulp.watch(int.src + '/pages/*.pug', ['html']);
+  gulp.watch(int.build + '/*.html').on('change', bs.reload);
+});
