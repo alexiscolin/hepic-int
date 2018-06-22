b7deba940f498a72cadc034c77250b235f6788a6
Alexis Colin <nesquimo@gmail.com>
2018-06-22 15:56:31 +0200

    Profil page style

--- int/build/assets/js/index.js
+++ int/build/assets/js/index.js
@@ -4,21 +4,13 @@
   console.log('hello');
 
   var elem = document.querySelector('.o-grid');
-  var msnry = void 0;
 
-  // imagesLoaded( elem, function() {
-  //   msnry = new Masonry(elem, {
-  //     itemSelector: '.o-tile-grided',
-  //     columnWidth: '.o-tile-grided',
-  //     fitWidth: true,
-  //     gutter: 20
-  //   });
-  // });
-  //
-  var grid = document.querySelector('.o-grid');
-  var colc = new Colcade(grid, {
-    columns: '.grid-col',
-    items: '.o-tile-grided'
-  });
+  if (elem) {
+    var msnry = void 0;
+    var colc = new Colcade(elem, {
+      columns: '.grid-col',
+      items: '.o-tile-grided'
+    });
+  }
 })();
 //# sourceMappingURL=index.js.map
--- int/src/assets/js/index.js
+++ int/src/assets/js/index.js
@@ -2,20 +2,13 @@
   console.log('hello');
 
   const elem = document.querySelector('.o-grid');
-  let msnry;
 
-  // imagesLoaded( elem, function() {
-  //   msnry = new Masonry(elem, {
-  //     itemSelector: '.o-tile-grided',
-  //     columnWidth: '.o-tile-grided',
-  //     fitWidth: true,
-  //     gutter: 20
-  //   });
-  // });
-  //
-  var grid = document.querySelector('.o-grid');
-  var colc = new Colcade( grid, {
-    columns: '.grid-col',
-    items: '.o-tile-grided'
-  });
+  if(elem){
+    let msnry;
+    let colc = new Colcade( elem, {
+      columns: '.grid-col',
+      items: '.o-tile-grided'
+    });
+  }
+
 })();
