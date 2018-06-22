3f1e110c1ae03b0e37144d161ad0ed45795ca8dd
Alexis Colin <nesquimo@gmail.com>
2018-06-22 12:15:59 +0200

    Mod: change Masonery -> Colcade

--- int/build/assets/js/index.js
+++ int/build/assets/js/index.js
@@ -6,13 +6,19 @@
   var elem = document.querySelector('.o-grid');
   var msnry = void 0;
 
-  imagesLoaded(elem, function () {
-    msnry = new Masonry(elem, {
-      itemSelector: '.o-tile-grided',
-      columnWidth: '.o-tile-grided',
-      fitWidth: true,
-      gutter: 20
-    });
+  // imagesLoaded( elem, function() {
+  //   msnry = new Masonry(elem, {
+  //     itemSelector: '.o-tile-grided',
+  //     columnWidth: '.o-tile-grided',
+  //     fitWidth: true,
+  //     gutter: 20
+  //   });
+  // });
+  //
+  var grid = document.querySelector('.o-grid');
+  var colc = new Colcade(grid, {
+    columns: '.grid-col',
+    items: '.o-tile-grided'
   });
 })();
 //# sourceMappingURL=index.js.map
--- int/src/assets/js/index.js
+++ int/src/assets/js/index.js
@@ -4,12 +4,18 @@
   const elem = document.querySelector('.o-grid');
   let msnry;
 
-  imagesLoaded( elem, function() {
-    msnry = new Masonry(elem, {
-      itemSelector: '.o-tile-grided',
-      columnWidth: '.o-tile-grided',
-      fitWidth: true,
-      gutter: 20
-    });
+  // imagesLoaded( elem, function() {
+  //   msnry = new Masonry(elem, {
+  //     itemSelector: '.o-tile-grided',
+  //     columnWidth: '.o-tile-grided',
+  //     fitWidth: true,
+  //     gutter: 20
+  //   });
+  // });
+  //
+  var grid = document.querySelector('.o-grid');
+  var colc = new Colcade( grid, {
+    columns: '.grid-col',
+    items: '.o-tile-grided'
   });
 })();
