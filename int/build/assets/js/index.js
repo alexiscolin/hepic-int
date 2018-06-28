14a933c2737603a5acc3ee1a473e108b0c8fbb56
Alexis Colin <nesquimo@gmail.com>
2018-06-28 17:48:41 +0200

    Minify js

--- int/build/assets/js/index.js
+++ int/build/assets/js/index.js
@@ -1,16 +1,8 @@
 'use strict';
 
 (function () {
-  console.log('hello');
-
-  var elem = document.querySelector('.o-grid');
-
-  if (elem) {
-    var msnry = void 0;
-    var colc = new Colcade(elem, {
-      columns: '.grid-col',
-      items: '.o-tile-grided'
-    });
+  var elem = document.querySelector('.o-grid');if (elem) {
+    var colc = new Colcade(elem, { columns: '.grid-col', items: '.o-tile-grided' });
   }
 })();
 //# sourceMappingURL=index.js.map
--- int/src/assets/js/index.js
+++ int/src/assets/js/index.js
@@ -1,14 +1,1 @@
-(function(){
-  console.log('hello');
-
-  const elem = document.querySelector('.o-grid');
-
-  if(elem){
-    let msnry;
-    let colc = new Colcade( elem, {
-      columns: '.grid-col',
-      items: '.o-tile-grided'
-    });
-  }
-
-})();
+(function(){const elem = document.querySelector('.o-grid');if(elem){let colc = new Colcade( elem, { columns: '.grid-col', items: '.o-tile-grided'});}})();
